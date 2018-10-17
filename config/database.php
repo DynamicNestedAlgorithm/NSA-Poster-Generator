78a6fd571d34346711bc88dc45fe4f4306293cf9
tjomega03 <37372913+tjomega03@users.noreply.github.com>
2018-10-17 16:42:04 +0000

    heroku deployment stuff

--- config/database.php
+++ config/database.php
@@ -14,7 +14,13 @@ return [
     */
 
     'default' => env('DB_CONNECTION', 'mysql'),
+    
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL")),
 
+    $host = $url["host"],
+    $username = $url["user"],
+    $password = $url["pass"],
+    $database = substr($url["path"], 1),
     /*
     |--------------------------------------------------------------------------
     | Database Connections
@@ -38,20 +44,20 @@ return [
             'prefix' => '',
         ],
 
-        'mysql' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-        ],
+        // 'mysql' => [
+        //     'driver' => 'mysql',
+        //     'host' => env('DB_HOST', '127.0.0.1'),
+        //     'port' => env('DB_PORT', '3306'),
+        //     'database' => env('DB_DATABASE', 'forge'),
+        //     'username' => env('DB_USERNAME', 'forge'),
+        //     'password' => env('DB_PASSWORD', ''),
+        //     'unix_socket' => env('DB_SOCKET', ''),
+        //     'charset' => 'utf8mb4',
+        //     'collation' => 'utf8mb4_unicode_ci',
+        //     'prefix' => '',
+        //     'strict' => true,
+        //     'engine' => null,
+        // ],
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', $host),
