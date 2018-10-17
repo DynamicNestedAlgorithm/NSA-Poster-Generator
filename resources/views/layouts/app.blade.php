0f0f450dfa71faddbd2a6852b69871f676f869d4
tjomega03 <37372913+tjomega03@users.noreply.github.com>
2018-10-17 09:25:22 +0000

    heroku deployment stuff

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'NSA Poster Generator') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -25,7 +25,7 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel the-nav">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                   <img src="{{asset('img/logo.png')}}" class= "logo"alt="logo" > {{ config('app.name', 'Laravel') }}
+                   <img src="{{asset('img/logo.png')}}" class= "logo"alt="logo" > {{ config('app.name', 'NSA Poster Generator') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
