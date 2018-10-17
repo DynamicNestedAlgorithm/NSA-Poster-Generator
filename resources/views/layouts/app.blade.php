44a4ead30e3b3dd52ad058b587e7aebea64357ef
tjomega03 <37372913+tjomega03@users.noreply.github.com>
2018-10-17 16:33:59 +0000

    heroku deployment stuff

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'NSA Poster Generator') }}</title>
+    <title>NSA Poster Generator</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -25,11 +25,11 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel the-nav">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                   <img src="{{asset('img/logo.png')}}" class= "logo"alt="logo" > {{ config('app.name', 'NSA Poster Generator') }}
+                   <img src="{{asset('img/logo.png')}}" class= "logo"alt="logo" > NSA Poster Generator
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                {{-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
-                </button>
+                </button> --}}
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
